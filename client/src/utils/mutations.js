a83fbfd10b486775e62ad8ee5fa853e3c50c50a1
Victoria Malie <vmalie3@gmail.com>
2022-07-28 14:27:18 -0500

    mutation.js

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,49 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  saveBook($authors: [String], $description: String!, $title: String!, $bookId: ID!, $image: String, $link: String) {
+    saveBook(authors: $authors, description: $description, title: $title, bookId: $bookId, image: $image, link: $link) {
+      token
+      book {
+        bookId
+        title
+        authors
+        description
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      savedBooks
+    }
+  }
+`;
