f7f0170187b51889db98e17a388067d24130ae9a
Victoria Malie <vmalie3@gmail.com>
2022-07-28 14:40:28 -0500

    searchBooks.js updates

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -5,6 +5,8 @@ import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
+import { SAVE_BOOK } from '../utils/queries';
+
 const SearchBooks = () => {
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
@@ -65,7 +67,7 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const response = await useMutation(SAVE_BOOK((bookToSave, token)));
 
       if (!response.ok) {
         throw new Error('something went wrong!');
